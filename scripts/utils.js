c2c318e5ab9396524f7d337b2ac9573a96dd53dc
Alexis Melendez <AlexisNoMain@icloud.com>
2023-07-11 10:55:41 -0400

    Agregando la funcion show

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,4 +2,8 @@ class Utils {
     static getNum(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    static show(issue) {
+        document.getElementById('issue').textContent = issue;
+    }
 }
